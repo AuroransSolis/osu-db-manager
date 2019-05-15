ca85934597342542a836156ef9f4725c4c80c7b0
Aurorans Solis <primalucegd@gmail.com>
2019-05-15 00:10:38 -0700

    Changed main a bit to estimate time for a single run.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate byteorder;
 
-pub mod databases;
+mod databases;
 mod deserialize_primitives;
 mod interactive;
 mod query;
 mod serialize_primitives;
+mod partial_databases;
 
 use std::fs::File;
+use std::time::Instant;
 
 use databases::osu::OsuDb;
 
 fn main() {
     let mut f = File::open("osu!.db").unwrap();
+    let start = Instant::now();
     let osudb = OsuDb::read_from_file(&mut f);
+    println!("Estimated time to run: {:?}", start.elapsed());
     if let Ok(osudb) = osudb {
         println!("Success!");
     } else {
