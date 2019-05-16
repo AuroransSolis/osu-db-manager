ff0718c84cd565d4a9a83a1d45e4e1fdd4c7bf48
Aurorans Solis <primalucegd@gmail.com>
2019-05-15 23:39:32 -0700

    Minor changes here. Nothing that important. Will update to reflect changes to db loading methods.

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,22 @@ mod deserialize_primitives;
 mod interactive;
 mod query;
 mod serialize_primitives;
-mod partial_databases;
+//mod partial_databases;
 
 use std::fs::File;
 use std::time::Instant;
+use std::io::Cursor;
+use std::sync::{Arc, Mutex, atomic::AtomicUsize};
+
+use byteorder::{ReadBytesExt, WriteBytesExt};
 
 use databases::osu::OsuDb;
+use databases::load::Load;
 
 fn main() {
-    let mut f = File::open("osu!.db").unwrap();
+    let mut f = File::open("auro-osu!.db").unwrap();
     let start = Instant::now();
-    let osudb = OsuDb::read_from_file(&mut f);
+    let osudb = OsuDb::read_from_file(&mut f, 1);
     println!("Estimated time to run: {:?}", start.elapsed());
     if let Ok(osudb) = osudb {
         println!("Success!");
