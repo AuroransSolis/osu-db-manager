52e4e45cad6786d55ad31ca41956bac2fe9df771
Aurorans Solis <primalucegd@gmail.com>
2019-05-21 10:27:34 -0700

    Made some changes to main to give some useful info and reflect changes to loading API.

--- src/main.rs
+++ src/main.rs
@@ -16,15 +16,27 @@ use byteorder::{ReadBytesExt, WriteBytesExt};
 
 use databases::osu::OsuDb;
 use databases::load::Load;
+use crate::databases::osu::OsuDbLoadSettings;
 
 fn main() {
     let mut f = File::open("auro-osu!.db").unwrap();
     let start = Instant::now();
-    let osudb = OsuDb::read_from_file(&mut f, 1);
+    let load_settings = OsuDbLoadSettings::new(1);
+    let osudb = OsuDb::read_from_file(load_settings, f);
     println!("Estimated time to run: {:?}", start.elapsed());
     if let Ok(osudb) = osudb {
-        println!("Success!");
+        println!("Success! Loaded in osu!.db with {} beatmaps.", osudb.number_of_beatmaps);
     } else {
         println!("Fuck.\n{:?}", osudb.unwrap_err());
+    }
+}
+
+pub fn clone_file(file: &File) -> File {
+    if cfg!(target_os = "linux") {
+        let rawfd = file.as_raw_fd();
+        unsafe { File::from_raw_fd(rawfd) }
+    } else {
+        let raw_handle = file.as_raw_handle();
+        unsafe { File::from_raw_handle(raw_handle) }
     }
 }
