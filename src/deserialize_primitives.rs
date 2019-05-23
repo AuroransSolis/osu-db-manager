f021a8fcb2178c6899193f0c20b3ce07dcd35fc9
Aurorans Solis <primalucegd@gmail.com>
2019-05-23 14:19:02 -0700

    haha surprise I changed the methods again

--- src/deserialize_primitives.rs
+++ src/deserialize_primitives.rs
@@ -1,11 +1,11 @@
 use byteorder::{ReadBytesExt, LittleEndian};
+use std::iter::Iterator;
 use std::fs::File;
 use std::io::{Result as IoResult, Error as IoError};
-use std::io::ErrorKind::InvalidData;
+use std::io::ErrorKind::{Other, InvalidData};
 use std::mem::size_of;
 use std::string::FromUtf8Error;
 use std::time::{Duration, SystemTime};
-
 // Primitive types we need to read from databases:
 // Byte
 // Short
@@ -16,36 +16,45 @@ use std::time::{Duration, SystemTime};
 // Double
 // Boolean
 // String
+// Datetime
 
-pub fn read_short(bytes: &[u8], cursor: &mut usize) -> i16 {
-    let ret = bytes[*cursor + 0] as i16 + ((bytes[*cursor + 1] as i16) << 8);
-    *cursor += 2;
-    ret
+const BYTE_ERR: IoError = IoError::new(Other, "Failed to read byte.");
+const SHORT_ERR: IoError = IoError::new(Other, "Failed to read byte for short.");
+const INT_ERR: IoError = IoError::new(Other, "Failed to read byte for int.");
+const LONG_ERR: IoError = IoError::new(Other, "Failed to read byte for long.");
+const ULEB128_ERR: IoError =IoError::new(Other, "Failed to read byte for ULEB128.");
+const SINGLE_ERR: IoError = IoError::new(Other, "Failed to read byte for single.");
+const DOUBLE_ERR: IoError = IoError::new(Other, "Failed to read byte for double.");
+const BOOLEAN_ERR: IoError = IoError::new(Other, "Failed to read byte for boolean.");
+const STRING_ERR: IoError = IoError::new(Other, "Failed to read indicator for string.");
+const DATETIME_ERR: IoError = IoError::new(Other, "Failed to read long for datetime.");
+
+pub fn read_byte<I: Iterator<Item= u8>>(i: &mut I) -> IoResult<u8> {
+    Ok(i.next().ok_or( BYTE_ERR)?)
 }
 
-pub fn read_int(bytes: &[u8], cursor: &mut usize) -> i32 {
-    let ret = bytes[*cursor + 0] as i32 + ((bytes[*cursor + 1] as i32) << 8)
-        + ((bytes[*cursor + 2] as i32) << 16) + ((bytes[*cursor + 3] as i32) << 24);
-    *cursor += 4;
-    ret
+pub fn read_short<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<i16> {
+    Ok(i.next().ok_or( SHORT_ERR)? as i16 + ((i.next().ok_or( SHORT_ERR)? as i16) << 8))
 }
 
-pub fn read_long(bytes: &[u8], cursor: &mut usize) -> i64 {
-    let ret = bytes[*cursor + 0] as i64 + ((bytes[*cursor + 1] as i64) << 8)
-        + ((bytes[*cursor + 2] as i64) << 16) + ((bytes[*cursor + 3] as i64) << 24)
-        + ((bytes[*cursor + 4] as i64) << 32) + ((bytes[*cursor + 5] as i64) << 40)
-        + ((bytes[*cursor + 6] as i64) << 48) + ((bytes[*cursor + 7] as i64) << 56);
-    *cursor += 8;
-    ret
+pub fn read_int<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<i32> {
+    Ok(i.next().ok_or( INT_ERR)? as i32 + ((i.next().ok_or( INT_ERR)? as i32) << 8)
+        + ((i.next().ok_or( INT_ERR)? as i32) << 16) + ((i.next().ok_or( INT_ERR)? as i32) << 24))
 }
 
-pub fn read_uleb128(bytes: &[u8], cursor: &mut usize) -> IoResult<usize> {
+pub fn read_long<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<i64> {
+    Ok(i.next().ok_or(LONG_ERR)? as i64 + ((i.next().ok_or(LONG_ERR)? as i64) << 8)
+        + ((i.next().ok_or(LONG_ERR)? as i64) << 16) + ((i.next().ok_or(LONG_ERR)? as i64) << 24)
+        + ((i.next().ok_or(LONG_ERR)? as i64) << 32) + ((i.next().ok_or(LONG_ERR)? as i64) << 40)
+        + ((i.next().ok_or(LONG_ERR)? as i64) << 48) + ((i.next().ok_or(LONG_ERR)? as i64) << 56))
+}
+
+pub fn read_uleb128<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<usize> {
     let mut out = 0;
     let mut found_end = false;
     let mut shift = 0;
-    let mut add = 0;
     while shift < size_of::<usize>() * 8 {
-        let b = bytes[*cursor + add];
+        let b = i.next().ok_or(ULEB128_ERR)?;
         if shift + 8 >= size_of::<usize>() * 8 {
             if 0b11111111 >> (size_of::<usize>() * 8 - shift) | b
                 < (0b10000000 >> size_of::<usize>() * 8 - shift - 1) {
@@ -54,8 +63,9 @@ pub fn read_uleb128(bytes: &[u8], cursor: &mut usize) -> IoResult<usize> {
                 break;
             } else {
                 let err_msg = format!("While the ULEB128 integer format supports integers \
-                    of arbitrary lengths, this program will only handle ULEB128 integers up to and \
-                    including {} bits in length.", size_of::<usize>() * 8);
+                    of arbitrary lengths, this program will only handle ULEB128 integers \
+                    representing integers up to and including {} bits in length.",
+                    size_of::<usize>() * 8);
                 return Err(IoError::new(InvalidData, err_msg.as_str()));
             }
         }
@@ -65,49 +75,76 @@ pub fn read_uleb128(bytes: &[u8], cursor: &mut usize) -> IoResult<usize> {
             break;
         }
         shift += 7;
-        add += 1;
     }
     if found_end {
-        *cursor += add;
         Ok(out)
     } else {
         let err_msg = format!("While the ULEB128 integer format supports integers \
-            of arbitrary lengths, this program will only handle ULEB128 integers up to and \
-            including {} bits in length.", size_of::<usize>() * 8);
+            of arbitrary lengths, this program will only handle ULEB128 integers representing \
+            integers up to and including {} bits in length.", size_of::<usize>() * 8);
         Err(IoError::new(InvalidData, err_msg.as_str()))
     }
 }
 
-pub fn read_single(bytes: &[u8], cursor: &mut usize) -> f32 {
-    let necessary = [bytes[*cursor + 3], bytes[*cursor + 2], bytes[*cursor + 1], bytes[*cursor]];
-    *cursor += 4;
-    unsafe { std::mem::transmute::<[u8; 4], f32>(necessary) }
+pub fn read_uleb128_with_len<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<(usize, usize)> {
+    let mut out = 0;
+    let mut found_end = false;
+    let mut shift = 0;
+    let mut len = 0;
+    while shift < size_of::<usize>() * 8 {
+        let b = i.next().ok_or(ULEB128_ERR)?;
+        if shift + 8 >= size_of::<usize>() * 8 {
+            if 0b11111111 >> (size_of::<usize>() * 8 - shift) | b
+                < (0b10000000 >> size_of::<usize>() * 8 - shift - 1) {
+                out += (b as usize) << shift;
+                found_end = true;
+                break;
+            } else {
+                let err_msg = format!("While the ULEB128 integer format supports integers \
+                    of arbitrary lengths, this program will only handle ULEB128 integers \
+                    representing integers up to and including {} bits in length.",
+                    size_of::<usize>() * 8);
+                return Err(IoError::new(InvalidData, err_msg.as_str()));
+            }
+        }
+        out += (b as usize & 0b01111111) << shift;
+        if !(b | 0b00000000) & 0b10000000 == 0b10000000 {
+            found_end = true;
+            break;
+        }
+        shift += 7;
+        len += 1;
+    }
+    if found_end {
+        Ok((out, len))
+    } else {
+        let err_msg = format!("While the ULEB128 integer format supports integers \
+            of arbitrary lengths, this program will only handle ULEB128 integers representing \
+            integers up to and including {} bits in length.", size_of::<usize>() * 8);
+        Err(IoError::new(InvalidData, err_msg.as_str()))
+    }
 }
 
-pub fn read_double(bytes: &[u8], cursor: &mut usize) -> f64 {
-    let necessary = [bytes[*cursor + 7], bytes[*cursor + 6], bytes[*cursor + 5], bytes[*cursor + 4],
-        bytes[*cursor + 3], bytes[*cursor + 2], bytes[*cursor + 1], bytes[*cursor]];
-    *cursor += 8;
-    unsafe { std::mem::transmute::<[u8; 8], f64>(necessary) }
+pub fn read_single<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<f32> {
+    Ok(f32::from_bits(read_int(i).map_err(|_| SINGLE_ERR)? as u32))
 }
 
-pub fn read_boolean(bytes: &[u8], cursor: &mut usize) -> bool {
-    let ret = bytes[cursor] == 0;
-    *cursor += 1;
-    ret
+pub fn read_double<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<f64> {
+    Ok(f64::from_bits(read_long(i).map_err(|_| DOUBLE_ERR)? as u64))
 }
 
-pub fn read_string_utf8(bytes: &[u8], cursor: &mut usize)
+pub fn read_boolean<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<bool> {
+    Ok(i.next().ok_or(BOOLEAN_ERR)? == 0)
+}
+
+pub fn read_string_utf8<I: Iterator<Item = u8>>(i: &mut I)
     -> IoResult<Result<String, FromUtf8Error>> {
-    let indicator = bytes[*cursor];
-    *cursor += 1;
+    let indicator = i.next().ok_or(STRING_ERR)?;
     if indicator == 0 {
         Ok(Ok(String::new()))
     } else if indicator == 0x0b {
-        let length = read_uleb128(bytes, cursor)?;
-        let ret = Ok(String::from_utf8(bytes[*cursor..*cursor + length].into_vec()));
-        *cursor += length;
-        ret
+        let length = read_uleb128(i)?;
+        Ok(String::from_utf8(i.take(length).collect::<Vec<_>>()))
     } else {
         let err_msg = format!("Found invalid indicator for string ({})", indicator);
         Err(IoError::new(InvalidData, err_msg.as_str()))
@@ -124,8 +161,8 @@ pub fn fromutf8_to_ioresult(r: Result<String, FromUtf8Error>, field: &str) -> Io
     }
 }
 
-pub fn read_datetime(bytes: &[u8], cursor: &mut usize) -> SystemTime {
-    let ticks = read_long(bytes, cursor);
+pub fn read_datetime<I: Iterator<Item = u8>>(i: &mut I) -> IoResult<SystemTime> {
+    let ticks = read_long(i).map_err(|_| DATETIME_ERR)?;
     let duration_since_epoch = Duration::from_micros(ticks as u64 / 10);
-    SystemTime::UNIX_EPOCH + duration_since_epoch
+    Ok(SystemTime::UNIX_EPOCH + duration_since_epoch)
 }
