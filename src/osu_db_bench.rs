b159c2f693a7a01aab15aed361c4a2ea3ca2c4f8
Aurorans Solis <primalucegd@gmail.com>
2019-05-29 11:43:45 -0700

    Added extra benchmarks for multithreaded loading. Will also have to check for correctness at some point.

--- src/osu_db_bench.rs
+++ src/osu_db_bench.rs
@@ -5,24 +5,70 @@ pub mod deserialize_primitives;
 
 use databases::osu::OsuDb;
 
-use criterion::Criterion;
-use std::fs::File;
+use criterion::{Criterion, black_box};
+use std::fs::read;
+use crate::databases::load::Load;
+
+const OSUDB_FILE: &str = "auro-osu!.db";
 
 criterion_group!{
     name = osu_db_bench;
     config = Criterion::default();
-    targets = bench_load_osudb
+    targets = bench_clone_database_vector, bench_load_osudb_1_job, bench_load_osudb_2_jobs,
+        bench_load_osudb_3_jobs, bench_load_osudb_4_jobs
 }
 
 criterion_main!{osu_db_bench}
 
-fn bench_load_osudb(c: &mut Criterion) {
-    c.bench_function("Load entire osu!.db", move |b| {
-        b.iter_with_setup(|| {
-            let file = File::open("borrowed-osu!.db").unwrap();
-            file
-        }, |mut file| {
-            if let Err(_) = OsuDb::read_from_file(&mut file) {
+fn bench_clone_database_vector(c: &mut Criterion) {
+    c.bench_function("Cloning osu!.db vector.", move |b| {
+        let file_bytes = read(OSUDB_FILE).unwrap();
+        b.iter(|| {
+            black_box(file_bytes.clone());
+        });
+    });
+}
+
+fn bench_load_osudb_1_job(c: &mut Criterion) {
+    c.bench_function("Load entire osu!.db (-j1)", move |b| {
+        let file_bytes = read(OSUDB_FILE).unwrap();
+        b.iter(|| {
+            if let Err(_) = OsuDb::read_from_bytes(1, file_bytes.clone()) {
+                panic!("Uh oh!");
+            }
+        });
+    });
+}
+
+
+
+fn bench_load_osudb_2_jobs(c: &mut Criterion) {
+    c.bench_function("Load entire osu!.db (-j2)", move |b| {
+        let file_bytes = read(OSUDB_FILE).unwrap();
+        b.iter(|| {
+            if let Err(_) = OsuDb::read_from_bytes(2, file_bytes.clone()) {
+                panic!("Uh oh!");
+            }
+        });
+    });
+}
+
+fn bench_load_osudb_3_jobs(c: &mut Criterion) {
+    c.bench_function("Load entire osu!.db (-j3)", move |b| {
+        let file_bytes = read(OSUDB_FILE).unwrap();
+        b.iter(|| {
+            if let Err(_) = OsuDb::read_from_bytes(1, file_bytes.clone()) {
+                panic!("Uh oh!");
+            }
+        });
+    });
+}
+
+fn bench_load_osudb_4_jobs(c: &mut Criterion) {
+    c.bench_function("Load entire osu!.db (-j4)", move |b| {
+        let file_bytes = read(OSUDB_FILE).unwrap();
+        b.iter(|| {
+            if let Err(_) = OsuDb::read_from_bytes(4, file_bytes.clone()) {
                 panic!("Uh oh!");
             }
         });
