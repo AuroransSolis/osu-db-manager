96e307ada2ba6b449cbe29b4ac6cbbef91d454d9
Aurorans Solis <primalucegd@gmail.com>
2019-05-29 17:36:20 -0700

    Macro'd making the benchmark functions and Criterion group.

--- src/osu_db_bench.rs
+++ src/osu_db_bench.rs
@@ -11,12 +11,40 @@ use crate::databases::load::Load;
 
 const OSUDB_FILE: &str = "jminn-osu!.db";
 
-criterion_group!{
+macro_rules! make_bench {
+    ($(($number:literal, $name:ident)),*) => {
+        $(
+            fn $name(c: &mut Criterion) {
+                let id = format!("Load entire osu!.db (-j{})", $number);
+                c.bench_function(id.as_str(), move |b| {
+                    let file_bytes = read(OSUDB_FILE).unwrap();
+                    b.iter(|| {
+                        if let Err(_) = OsuDb::read_from_bytes($number, file_bytes.clone()) {
+                            panic!("Uh oh!");
+                        }
+                    });
+                });
+            }
+        )*
+        criterion_group!{
+            name = osu_db_bench;
+            config = Criterion::default();
+            targets = bench_clone_database_vector, $($name),*
+        }
+    }
+}
+
+make_bench!{
+    (1, load_osudb_jobs_1), (2, load_osudb_jobs_2), (3, load_osudb_jobs_3), (4, load_osudb_jobs_4),
+    (5, load_osudb_jobs_5), (6, load_osudb_jobs_6), (7, load_osudb_jobs_7), (8, load_osudb_jobs_8)
+}
+
+/*criterion_group!{
     name = osu_db_bench;
     config = Criterion::default();
     targets = bench_clone_database_vector, bench_load_osudb_1_job, bench_load_osudb_2_jobs,
         bench_load_osudb_3_jobs, bench_load_osudb_4_jobs
-}
+}*/
 
 criterion_main!{osu_db_bench}
 
@@ -25,50 +53,6 @@ fn bench_clone_database_vector(c: &mut Criterion) {
         let file_bytes = read(OSUDB_FILE).unwrap();
         b.iter(|| {
             black_box(file_bytes.clone());
-        });
-    });
-}
-
-fn bench_load_osudb_1_job(c: &mut Criterion) {
-    c.bench_function("Load entire osu!.db (-j1)", move |b| {
-        let file_bytes = read(OSUDB_FILE).unwrap();
-        b.iter(|| {
-            if let Err(_) = OsuDb::read_from_bytes(1, file_bytes.clone()) {
-                panic!("Uh oh!");
-            }
-        });
-    });
-}
-
-fn bench_load_osudb_2_jobs(c: &mut Criterion) {
-    c.bench_function("Load entire osu!.db (-j2)", move |b| {
-        let file_bytes = read(OSUDB_FILE).unwrap();
-        b.iter(|| {
-            if let Err(_) = OsuDb::read_from_bytes(2, file_bytes.clone()) {
-                panic!("Uh oh!");
-            }
-        });
-    });
-}
-
-fn bench_load_osudb_3_jobs(c: &mut Criterion) {
-    c.bench_function("Load entire osu!.db (-j3)", move |b| {
-        let file_bytes = read(OSUDB_FILE).unwrap();
-        b.iter(|| {
-            if let Err(_) = OsuDb::read_from_bytes(1, file_bytes.clone()) {
-                panic!("Uh oh!");
-            }
-        });
-    });
-}
-
-fn bench_load_osudb_4_jobs(c: &mut Criterion) {
-    c.bench_function("Load entire osu!.db (-j4)", move |b| {
-        let file_bytes = read(OSUDB_FILE).unwrap();
-        b.iter(|| {
-            if let Err(_) = OsuDb::read_from_bytes(4, file_bytes.clone()) {
-                panic!("Uh oh!");
-            }
         });
     });
 }
