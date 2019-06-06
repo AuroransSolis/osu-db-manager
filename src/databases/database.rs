91c2f56ed8637271a61f343c69ad70937c5ff441
Aurorans Solis <primalucegd@gmail.com>
2019-06-06 09:44:12 -0700

    Just a bit of fixing for the stuff and things.

--- src/databases/database.rs
+++ src/databases/database.rs
@@ -2,18 +2,42 @@ use std::io::Result as IoResult;
 use crate::databases::{osu::OsuDb, collection::CollectionDb, scores::ScoresDb, load::Load};
 use crate::argument::Database;
 
+#[derive(Debug)]
 pub enum OsuDatabase {
     Osu(OsuDb),
     Collection(CollectionDb),
     Scores(ScoresDb)
 }
 
+use self::OsuDatabase::*;
+
 impl OsuDatabase {
-    pub fn read_from_bytes<Db: LoadDb>(jobs: usize, db: Database) -> IoResult<Self> {
-        match db {
-            Database::OsuDb(bytes) => OsuDb::read_from_bytes(jobs, bytes),
-            Database::CollectionDb(bytes) => CollectionDb::read_from_bytes(jobs, bytes),
-            Database::ScoresDb(bytes) => ScoresDb::read_from_bytes(jobs, bytes)
+    pub fn read_from_bytes(jobs: usize, db: Database) -> IoResult<Self> {
+        Ok(match db {
+            Database::OsuDb(b) => Osu(OsuDb::read_from_bytes(jobs, b)?),
+            Database::CollectionDb(b) => Collection(CollectionDb::read_from_bytes(jobs, b)?),
+            Database::ScoresDb(b) => Scores(ScoresDb::read_from_bytes(jobs, b)?)
+        })
+    }
+
+    pub fn unwrap_osu(self) -> OsuDb {
+        match self {
+            Osu(osu) => osu,
+            _ => unreachable!()
+        }
+    }
+
+    pub fn unwrap_collection(self) -> CollectionDb {
+        match self {
+            Collection(collection) => collection,
+            _ => unreachable!()
+        }
+    }
+
+    pub fn unwrap_scores(self) -> ScoresDb {
+        match self {
+            Scores(scores) => scores,
+            _ => unreachable!()
         }
     }
 }
