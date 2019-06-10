089dc5ef1fae57d9a018687a2cf7df6e870de21b
Aurorans Solis <primalucegd@gmail.com>
2019-06-10 12:32:20 -0700

    Added scores.db benchmarking.

--- src/osu_db_bench.rs
+++ src/osu_db_bench.rs
@@ -1,20 +1,22 @@
 #[macro_use] extern crate criterion;
 extern crate num_cpus;
 
-pub mod databases;
-pub mod deserialize_primitives;
+mod databases;
+mod deserialize_primitives;
+mod argument;
 
-use crate::databases::{osu::OsuDb, load::Load};
+use crate::databases::{osu::OsuDb, scores::ScoresDb, load::Load};
 
 use criterion::{Criterion, black_box};
 use std::fs::read;
 
 const OSUDB_FILE: &str = "tama-osu!.db";
+const SCORESDB_FILE: &str = "old-auro-scores.db";
 
 macro_rules! make_bench {
-    ($(($number:literal, $name:ident)),*) => {
+    ($(($number:literal, $osu_name:ident, $scores_name:ident)),*) => {
         $(
-            fn $name(c: &mut Criterion) {
+            fn $osu_name(c: &mut Criterion) {
                 let id = format!("Load entire osu!.db (-j{})", $number);
                 c.bench_function(id.as_str(), move |b| {
                     let file_bytes = read(OSUDB_FILE).unwrap();
@@ -25,11 +27,23 @@ macro_rules! make_bench {
                     });
                 });
             }
+            
+            fn $scores_name(c: &mut Criterion) {
+                let id = format!("Load entire scores.db (-j{})", $number);
+                c.bench_function(id.as_str(), move |b| {
+                    let file_bytes = read(SCORESDB_FILE).unwrap();
+                    b.iter_with_setup(|| file_bytes.clone(), |fb| {
+                        if let Err(_) = ScoresDb::read_from_bytes($number, fb) {
+                            panic!("Uh oh!");
+                        }
+                    });
+                });
+            }
         )*
         criterion_group!{
             name = osu_db_bench;
             config = Criterion::default();
-            targets = bench_open_database_file, $($name),*
+            targets = bench_open_osudb_file, bench_open_scoresdb_file, $($osu_name, $scores_name),*
         }
     }
 }
@@ -37,21 +51,42 @@ macro_rules! make_bench {
 // Uncomment up to the number of threads available on your CPU
 // Yes, I could make a proc macro for this, but I'm feeling lazy so no.
 make_bench!{
-    (1, load_osudb_jobs_1), (2, load_osudb_jobs_2), (3, load_osudb_jobs_3), (4, load_osudb_jobs_4)
+    (1, load_osudb_jobs_1, load_scoresdb_jobs_1),
+    (2, load_osudb_jobs_2, load_scoresdb_jobs_2),
+    (3, load_osudb_jobs_3, load_scoresdb_jobs_3),
+    (4, load_osudb_jobs_4, load_scoresdb_jobs_4),
+    (5, load_osudb_jobs_5, load_scoresdb_jobs_5),
+    (6, load_osudb_jobs_6, load_scoresdb_jobs_6),
+    (7, load_osudb_jobs_7, load_scoresdb_jobs_7),
+    (8, load_osudb_jobs_8, load_scoresdb_jobs_8)
     /*
-    (5, load_osudb_jobs_5), (6, load_osudb_jobs_6), (7, load_osudb_jobs_7), (8, load_osudb_jobs_8),
-    (9, load_osudb_jobs_9), (10, load_osudb_jobs_10), (11, load_osudb_jobs_11),
-    (12, load_osudb_jobs_12), (13, load_osudb_jobs_13), (14, load_osudb_jobs_14),
-    (15, load_osudb_jobs_15), (16, load_osudb_jobs_16)
+    (9, load_osudb_jobs_9, load_scoresdb_jobs_9),
+    (10, load_osudb_jobs_10, load_scoresdb_jobs_10),
+    (11, load_osudb_jobs_11, load_scoresdb_jobs_11),
+    (12, load_osudb_jobs_12, load_scoresdb_jobs_12),
+    (13, load_osudb_jobs_13, load_scoresdb_jobs_13),
+    (14, load_osudb_jobs_14, load_scoresdb_jobs_14),
+    (15, load_osudb_jobs_15, load_scoresdb_jobs_15),
+    (16, load_osudb_jobs_16, load_scoresdb_jobs_16)
     */
 }
 
 criterion_main!{osu_db_bench}
 
-fn bench_open_database_file(c: &mut Criterion) {
+fn bench_open_osudb_file(c: &mut Criterion) {
     c.bench_function("Opening osu!.db file and reading to vector", move |b| {
         b.iter(|| {
             if let Err(_) = read(OSUDB_FILE) {
+                panic!("Uh oh!");
+            }
+        });
+    });
+}
+
+fn bench_open_scoresdb_file(c: &mut Criterion) {
+    c.bench_function("Opening scores.db and reading to vector", move |b| {
+        b.iter(|| {
+            if let Err(_) = read(SCORESDB_FILE) {
                 panic!("Uh oh!");
             }
         });
